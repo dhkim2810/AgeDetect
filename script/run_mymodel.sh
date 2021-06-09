ed00c053043a385ef46357e65fd999f320934b65
Chuck Kim <dhkim2810@gmail.com>
2021-06-09 02:43:14 +0000

    fix script

--- script/run_mymodel.sh
+++ script/run_mymodel.sh
@@ -1,9 +1,9 @@
-python /root/volume/AgeDetect/main.py \
-        --use_gpu --cudnn --trial 16 --arch spinalresnet18 \
-        --optim sgd --learning_rate 1e-4 --nesterov \
-        --scheduler multi_step --milestone 240 320 380 \
-        --epochs 400 --img_size 180 --da
-python /root/volume/AgeDetect/eval.py \
-        --use_gpu --eval --trial 16 --arch spinalresnet18 \
-        --img_size 180
-python /root/volume/AgeDetect/plot.py --arch spinalresnet18 --trial 16
+python /root/volume/AgeDetect/rvc_main.py \
+        --use_gpu --trial 7 --cudnn --arch random_bin \
+        --optim adam --learning_rate 2e-4 \
+        --scheduler multi_step --milestone 70 150 240 280 --gamma 0.5 \
+        --epochs 300 --img_size 224 --da --batch_size 128
+python /root/volume/AgeDetect/rvc_eval.py \
+        --eval --use_gpu --trial 7 --cudnn --arch random_bin \
+        --img_size 224
+python /root/volume/AgeDetect/rvc_plot.py --arch random_bin --trial 7
--- script/run_randombin.sh
+++ script/run_randombin.sh
@@ -1,9 +1,9 @@
 python /root/volume/AgeDetect/rvc_main.py \
-        --use_gpu --trial 4 --cudnn --arch random_bin \
-        --optim adam \
-        --scheduler multi_step --milestone 180 240 280 --gamma 0.1 \
-        --epochs 300 --img_size 224 --da --thumbnail --batch_size 64
+        --use_gpu --trial 6 --cudnn --arch random_bin \
+        --optim adam --learning_rate 2e-4 \
+        --scheduler multi_step --milestone 70 150 240 280 --gamma 0.5 \
+        --epochs 300 --img_size 224 --da --batch_size 64
 python /root/volume/AgeDetect/rvc_eval.py \
-        --eval --use_gpu --trial 4 --cudnn --arch random_bin \
+        --eval --use_gpu --trial 6 --cudnn --arch random_bin \
         --img_size 224
-python /root/volume/AgeDetect/rvc_plot.py --arch random_bin --trial 4
+python /root/volume/AgeDetect/rvc_plot.py --arch random_bin --trial 6
